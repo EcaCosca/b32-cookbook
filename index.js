cf8727c44a5a080f6151001116ba2be32f6b1502
EcaCosca <enriquecoscarelli@gmail.com>
2022-10-07 16:27:35 +0200

    Done for now

---
+++ index.js
@@ -0,0 +1,15 @@
+const handleclickCoffee = () => {
+    document.querySelector('#rec-show').src = "./recepies/coffee.html"
+}
+
+const handleclickFrap = () => {
+    document.querySelector('#rec-show').src = "./recepies/frap.html"
+}
+
+const handleclickMac = () => {
+    document.querySelector('#rec-show').src = "./recepies/macciato.html"
+}
+
+document.querySelector('#coffe-button').addEventListener('click', handleclickCoffee)
+document.querySelector('#frapu-button').addEventListener('click', handleclickFrap)
+document.querySelector('#mac-button').addEventListener('click', handleclickMac)
